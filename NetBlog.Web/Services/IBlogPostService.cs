b9e182a655b7ac2700f872195097d371b1ebd3b9
Hứa Tiên Hào <huatienhao095@gmail.com>
2024-05-21 10:39:43 +0700

    Add Home pagination

--- NetBlog.Web/Services/IBlogPostService.cs
+++ NetBlog.Web/Services/IBlogPostService.cs
@@ -4,11 +4,16 @@ namespace NetBlog.Web.Services
 {
     public interface IBlogPostService
     {
-        Task<IEnumerable<BlogPost>> GetAllAsync(string? searchQuery = null, string? searchByTag = null);
+        Task<IEnumerable<BlogPost>> GetAllAsync(
+            string? searchQuery = null, 
+            string? searchByTag = null,
+            int pageNumber = 1,
+            int pageSize = 100);
         Task<BlogPost?> GetAsync(Guid id);
         Task<BlogPost> AddAsync(BlogPost blogPost);
         Task<BlogPost?> UpdateAsync(BlogPost blogPost);
         Task<BlogPost?> DeleteAsync(Guid id);
         Task<BlogPost?> GetByUrlHandelAsync(string urlHandle); 
+        Task<int> CountAsync();
     }
 }
