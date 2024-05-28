50c1c0a43aaedb2cba93b55074ad6f5d17290983
Hứa Tiên Hào <huatienhao095@gmail.com>
2024-05-28 14:31:09 +0700

    Add Tag Modal and Center Edit Tag

---
+++ NetBlog.Web/Models/ViewModels/ModalTagRequests.cs
@@ -0,0 +1,10 @@
+﻿using NetBlog.Web.Models.Domain;
+
+namespace NetBlog.Web.Models.ViewModels
+{
+    public class ModalTagRequests
+    {
+        public IEnumerable<Tag> Tags { get; set; } = new List<Tag>();
+        public AddTagRequest AddTagRequest { get; set; }
+    }
+}
